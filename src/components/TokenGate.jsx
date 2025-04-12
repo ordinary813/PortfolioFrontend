6e29611f2270740150286343db4dcf5e3c6b8d55
ordinary813 <ordinaryperson813@gmail.com>
2025-04-13 00:21:30 +0300

    minor import fix 2

--- src/components/TokenGate.jsx
+++ src/components/TokenGate.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import axios from 'axios';
-import API_BASE_URL from './api.js';
+import API_BASE_URL from '../api.js';
 
 const TokenGate = ({ onUnlock }) => {
   const [token, setToken] = useState('');
